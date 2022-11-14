cf4067915faccdaf0e3d02d88a81a8337caec115
NghiaTran0812 <garenpc2003@gmail.com>
2022-11-14 14:01:58 +0700

    Revert "Create Spawn Tombstone system with random position, rotation and scale"
    This reverts commit 51297c7f5daa56b4aa029805bfbe707d0663409e.

--- Assets/Scripts/ComponentsAndTags/GraveyardAspect.cs
+++ Assets/Scripts/ComponentsAndTags/GraveyardAspect.cs
@@ -1,5 +1,4 @@
 ﻿using Unity.Entities;
-using Unity.Mathematics;
 using Unity.Transforms;
 
 public readonly partial struct GraveyardAspect : IAspect
@@ -9,41 +8,5 @@ public readonly partial struct GraveyardAspect : IAspect
     private readonly TransformAspect _transfromAspect;
 
     private readonly RefRO<GraveyardProperties> _graveyardProperties;
-    private readonly RefRW<GraveyardRandom> _graveyardRandom;
-    
-
-    public int NumberTombstoneToSpawn => _graveyardProperties.ValueRO.NumberToSpawn;
-    public Entity TombstonePrefab => _graveyardProperties.ValueRO.TombstonePrefab;
-
-    public UniformScaleTransform GetRandomTombstoneTransform()
-    {
-        return new()
-        {
-            Position = GetRandomPosition(),
-            Rotation = GetRandomQuaternion(),
-            Scale = GetRandomScale(0.2f)
-        };
-    }
-
-    private float3 GetRandomPosition()
-    {
-        float3 randomPosition;
-        do
-        {
-            randomPosition = _graveyardRandom.ValueRW.Value.NextFloat3(MinCorner, MaxCorner);
-        } while (math.distancesq(_transfromAspect.Position, randomPosition) <= BRAIN_SAFETY_RADIUS_SQ);
-        return randomPosition;
-    }
-    private float3 MinCorner => _transfromAspect.Position - HalfDimension;
-    private float3 MaxCorner => _transfromAspect.Position + HalfDimension;
-    private float3 HalfDimension => new() { 
-        x = _graveyardProperties.ValueRO.FieldDimensions.x * 0.5f,
-        y = 0,
-        z = _graveyardProperties.ValueRO.FieldDimensions.y * 0.5f
-    };
-
-    private const float BRAIN_SAFETY_RADIUS_SQ = 100f;
-    private quaternion GetRandomQuaternion() => quaternion.RotateY(_graveyardRandom.ValueRW.Value.NextFloat(-0.5f, 0.5f));
-    private float GetRandomScale(float min) => _graveyardRandom.ValueRW.Value.NextFloat(min, 1);
-
+    private readonly RefRW<GraveyardRandom> _graveyardRandom;   
 }
